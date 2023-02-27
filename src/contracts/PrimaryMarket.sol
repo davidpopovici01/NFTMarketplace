cc5ffa28be741859c80794cf4fdb41b76f95d2c5
David Popovici <d719@ic.ac.uk>
2023-02-27 03:27:38 +0000

    added primaryMarket.sol

---
+++ src/contracts/PrimaryMarket.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "../interfaces/ITicketNFT.sol";
+import "../interfaces/IPrimaryMarket.sol";
+import "../interfaces/IERC20.sol";
+import "./TicketNFT.sol";
+
+contract PrimaryMarket is IPrimaryMarket {
+    address public _admin;
+    IERC20 public immutable _purchaseToken;
+    uint256 public immutable TOKEN_PRICE = 100e18;
+    uint256 public immutable TOKEN_LIMIT = 1000;
+    ITicketNFT public _ticketNFT;
+    uint256 public minted = 0;
+
+    constructor(IERC20 purchaseToken) {
+        _admin = msg.sender;
+        _purchaseToken = purchaseToken;
+        _ticketNFT = new TicketNFT(address(this));
+    }
+
+    function admin() external view returns (address) {
+        return _admin;
+    }
+
+    function getTicketNFT() external view returns (address) {
+        return address(_ticketNFT);
+    }
+
+    function purchase(string memory holderName) external {
+        require(minted < TOKEN_LIMIT, "no more tickets can be purchased");
+        require(
+            _purchaseToken.balanceOf(msg.sender) >= TOKEN_PRICE,
+            "insufficient balance"
+        );
+        _purchaseToken.transferFrom(msg.sender, _admin, TOKEN_PRICE);
+        _ticketNFT.mint(msg.sender, holderName);
+        minted += 1;
+        emit Purchase(msg.sender, holderName);
+    }
+}
